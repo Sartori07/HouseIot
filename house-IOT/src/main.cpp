27245c915867cbfb524b0b9faabca88d0ca8092a
Sartori07 <sartori.lean@gmail.com>
2022-04-13 15:45:38 -0400

    test Sensor PIR

--- house-IOT/src/main.cpp
+++ house-IOT/src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
 
-void setup() {
-  Serial.begin(9600);
+#define acionarele 8 
+#define LesensorPIR 7
+int LeituraSensor; 
+ 
+void setup()
+{
+   pinMode(acionarele, OUTPUT); 
+   pinMode(LesensorPIR, INPUT); 
+   Serial.begin(9600);
 }
-
-void loop() {
-  Serial.println("Hello LabMaker");
-  delay(1000);
+ 
+void loop()
+{
+  //Le o sensor de presenca
+  LeituraSensor = digitalRead(LesensorPIR);
+ 
+  //Nao ha deteccao de movimento
+   if (LeituraSensor == LOW) 
+   {
+     Serial.println("Nada");
+   }
+   //Ha movimento detectado - acende lampada
+   else 
+   {
+      Serial.println("Presença");
+   }
+   delay(1000);
 }
